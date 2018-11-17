639ce772a9e7a53167d71a27631e0344860549e6
Rafael Jaber <rafael.jaber@gmail.com>
2018-11-17 16:01:37 -0200

    Implementado o break, case use ele, não será executado o else

--- python/Loop_WHILE.py
+++ python/Loop_WHILE.py
@@ -6,8 +6,12 @@ while i < 10:
     if i == 5:
         i = i + 1
         continue
+
+    if i == 8:
+        break
+
     print(i)
     i = i + 1
-
+#Caso dé o break a interação é finalizada ou seja, não vai executar o else.
 else:
     print("Fim do loop")
