4fcff28db0edc29687c95e9b0a6bd7a7a79d27a4
Rafael Jaber <rafael.jaber@gmail.com>
2018-11-17 17:14:58 -0200

    Criado arquivo da aula sobre funçoes

--- python/Funcoes.py
+++ python/Funcoes.py
@@ -3,3 +3,11 @@ def calcula_dobro(numero):
     return numero * 2
 
 print(calcula_dobro(15))
+
+print('')
+
+def calcula_soma_numeros(**numeros):
+    print(numeros)
+    return sum(numeros.values())
+
+print(calcula_soma_numeros(num1=5,num2=10,num3=20))
