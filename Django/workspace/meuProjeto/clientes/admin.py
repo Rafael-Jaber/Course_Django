9649b81a7df34fce781233fa5cf9268ac6cd4363
Rafael Jaber <rafael.jaber@gmail.com>
2018-11-18 11:50:50 -0200

    Configurações automáticas django

--- Django/workspace/meuProjeto/clientes/admin.py
+++ Django/workspace/meuProjeto/clientes/admin.py
@@ -3,7 +3,13 @@ from .models import Cliente, Telefone, CPF, Departamento
 
 # Register your models here
 
-admin.site.register(Cliente)
+class EmpregadoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'endereco', 'email')
+    list_filter = ('departamentos', )
+    search_fields = ('id', 'nome')
+
+
+admin.site.register(Cliente, EmpregadoAdmin)
 admin.site.register(Telefone)
 admin.site.register(CPF)
 admin.site.register(Departamento)
