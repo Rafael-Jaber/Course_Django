5d7f7d45d75133215d32db25a0c3c98ab3f431f4
Rafael Jaber <rafael.jaber@gmail.com>
2018-11-18 00:49:41 -0200

    Configurado os relacionamentos entre as tabelas

--- Django/workspace/meuProjeto/clientes/admin.py
+++ Django/workspace/meuProjeto/clientes/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Cliente, Telefone
+from .models import Cliente, Telefone, CPF, Departamento
 
 # Register your models here
 
 admin.site.register(Cliente)
 admin.site.register(Telefone)
+admin.site.register(CPF)
+admin.site.register(Departamento)
--- Django/workspace/meuProjeto/clientes/models.py
+++ Django/workspace/meuProjeto/clientes/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+class Departamento(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+class CPF(models.Model):
+    numero = models.CharField(max_length=15)
+    data_exp = models.DateTimeField(auto_now=False)
+
+    def __str__(self):
+        return self.numero
 
 
 class Cliente(models.Model):
@@ -8,6 +21,8 @@ class Cliente(models.Model):
     salario = models.DecimalField(max_digits=10, decimal_places=2)
     idade = models.IntegerField()
     email = models.EmailField()
+    cpf = models.OneToOneField(CPF, on_delete=models.CASCADE, blank=True, null=True)
+    departamentos = models.ManyToManyField(Departamento)
 
     def __str__(self):
         return self.nome
@@ -20,3 +35,5 @@ class Telefone(models.Model):
 
     def __str__(self):
         return self.cliente.nome + ' - ' + self.descricao + ' - ' + self.numero
+
+
